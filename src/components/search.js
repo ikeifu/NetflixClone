9ae78fc8f268a0d82c7e5b47fffe4ab6a9d14052
Keith <fungsenkeith@gmail.com>
2021-10-17 14:30:32 -0400

    refactor

--- src/components/search.js
+++ src/components/search.js
@@ -1,22 +1,26 @@
-import React, { useState, useEffect } from 'react'
-const Search = ({movieTitle, setMovieTitle}) => {
-    const [currentMovie,setCurrentMovie] = useState("")
-    const submitButtonHandler = () => {
-        setMovieTitle(currentMovie)
-    }
-    return (
+import React, { useState, useEffect } from "react";
+const Search = ({ movieTitle, setMovieTitle }) => {
+  const [currentMovie, setCurrentMovie] = useState("");
+  const submitButtonHandler = () => {
+    setMovieTitle(currentMovie);
+  };
+  return (
     <>
-    <span>Search for movies</span>
-    <input
-    type="text"
-    value={currentMovie}
-    id="header-search"
-    placeholder="Movie Title"
-    name="s"
-    onChange={(e) => setCurrentMovie(e.target.value)} 
-    />
-    <button type="submit" onClick={() => submitButtonHandler()}> Search </button>
-    </>)
-}
+      <span>Search for movies</span>
+      <input
+        type="text"
+        value={currentMovie}
+        id="header-search"
+        placeholder="Movie Title"
+        name="s"
+        onChange={(e) => setCurrentMovie(e.target.value)}
+      />
+      <button type="submit" onClick={() => submitButtonHandler()}>
+        {" "}
+        Search{" "}
+      </button>
+    </>
+  );
+};
 
-export default Search
+export default Search;
